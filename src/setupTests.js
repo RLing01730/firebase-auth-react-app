6f4700cb9da6e8696c7d55d33de8f74fa638b229
Richard Ling <rling505@gmail.com>
2025-11-23 20:11:02 -0500

    Add Firebase SDK mocks for tests
    Mock Firebase SDK methods for testing.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,15 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// src/setupTests.js
+// Runs before all tests in Create React App to mock Firebase SDK
+
+jest.mock('firebase/app', () => ({
+  initializeApp: jest.fn(),
+  getApps: jest.fn(() => []),
+}));
+
+jest.mock('firebase/auth', () => ({
+  getAuth: jest.fn(() => ({})),
+  onAuthStateChanged: jest.fn((auth, cb) => { cb(null); return () => {}; }),
+  signInWithEmailAndPassword: jest.fn(() => Promise.resolve({ user: { uid: 'test' } })),
+  createUserWithEmailAndPassword: jest.fn(() => Promise.resolve({ user: { uid: 'test' } })),
+  signOut: jest.fn(() => Promise.resolve()),
+}));
