8f2906d8efc48cab276cb07d1bee6b89df84eb4a
Richard E. Ling <rling505@gmail.com>
2025-11-23 20:04:00 -0500

    Update firebase.js configuration

--- src/firebase.js
+++ src/firebase.js
@@ -11,12 +11,12 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyCxmWFl-Malu9iOPu6g0YUtyTbUUNkJCc8",
+  authDomain: "fir-auth-react-app-14411.firebaseapp.com",
+  projectId: "fir-auth-react-app-14411",
+  storageBucket: "fir-auth-react-app-14411.firebasestorage.app",
+  messagingSenderId: "795514596116",
+  appId: "1:795514596116:web:a9246d15782229a487b001"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
